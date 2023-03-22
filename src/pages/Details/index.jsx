d94d9105104dae1a6195a1421d8cd2bf5039a0cb
Hélio Lúcio <helio.lucio.jr@hotmail.com>
2023-03-22 18:50:52 -0300

    frontend 99% finalizado

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -5,40 +5,88 @@ import { Button } from "../../components/Button"
 import { Section } from "../../components/Section"
 import { Tag } from "../../components/Tag"
 import { ButtonText } from "../../components/ButtonText"
+import { useNavigate, useParams } from "react-router-dom"
+import { useEffect, useState } from "react"
+import { api } from "../../services/api"
 
 export function Details() {
+  const [data, setData] = useState(null)
+
+  const params = useParams()
+  const navigate = useNavigate()
+
+  function handleBack() {
+    navigate(-1)
+  }
+
+  async function handleRemove() {
+    const confirm = window.confirm("Deseja realmente remover a nota?")
+
+    if (confirm) {
+      await api.delete(`/notes/${params.id}`)
+      navigate(-1)
+    }
+  }
+
+  useEffect(() => {
+    async function fetchNote() {
+      const response = await api.get(`/notes/${params.id}`)
+      setData(response.data)
+    }
+
+    fetchNote()
+  }, [])
+
   return (
     <Container>
       <Header />
 
-      <main>
-        <Content>
-          <ButtonText title="Excluir Nota" />
+      {
+        data &&
+        <main>
+          <Content>
+            <ButtonText title="Excluir Nota" onClick={handleRemove} />
 
-          <h1>Introdução ao React</h1>
+            <h1>{data.title}</h1>
 
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas nisi reprehenderit non. Atque debitis sint qui beatae iste commodi tempora vel reiciendis similique porro distinctio, quaerat accusantium ab ipsam non.
-            Sunt aliquam perspiciatis laborum? Ullam odio totam quibusdam, perspiciatis, libero a, sapiente esse et iste alias aspernatur consequuntur molestias beatae? Ducimus vero beatae totam delectus quod neque inventore, harum aspernatur?
-            Cupiditate aperiam veritatis deserunt accusamus optio, asperiores aliquam doloribus obcaecati temporibus debitis iure tempora, similique nisi dignissimos, veniam quis sequi facilis incidunt ea. Consequuntur nam voluptatem corporis ad et iusto!
-            Sit ipsum officiis sed veritatis sunt quos similique voluptatum iure a inventore reprehenderit possimus delectus temporibus mollitia blanditiis quam voluptate id, eius laudantium fugit. Unde nihil facilis odit distinctio iusto!
-            Voluptate cum nulla omnis sapiente quibusdam labore eveniet voluptatum? Distinctio mollitia ex beatae libero? Atque totam, quam, obcaecati ipsam ipsum, in pariatur veniam assumenda nostrum sunt fugit quo autem! Dolorum.
-          </p>
+            <p>{data.description}</p>
 
-          <Section title="Links úteis">
-            <Links>
-              <li><a href="#">https://www.rocketseat.com.br/</a></li>
-              <li><a href="#">https://www.rocketseat.com.br/</a></li>
-            </Links>
-          </ Section>
+            {
+              data.links &&
+              <Section title="Links úteis">
+                <Links>
+                  {
+                    data.links.map(link => (
+                      <li key={String(link.id)}>
+                        <a href={link.url} target="_blank">
+                          {link.url}
+                        </a>
+                      </li>
+                    ))
+                  }
+                </Links>
+              </ Section>
+            }
 
-          <Section title="Marcadores">
-            <Tag title="express" />
-            <Tag title="node" />
-          </ Section>
+            {
+              data.tags &&
+              <Section title="Marcadores">
+                {
+                  data.tags.map(tag => (
+                    <Tag
+                      key={String(tag.id)}
+                      title={tag.name}
+                    />
+                  ))
+                }
+              </ Section>
 
-          <Button title="Voltar" />
-        </Content>
-      </main>
+            }
+            <Button title="Voltar" onClick={handleBack} />
+          </Content>
+        </main>
+      }
+
     </Container>
   )
 }
--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,4 +1,5 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 import { Header } from '../../components/Header'
 import { Input } from '../../components/Input'
@@ -6,10 +7,69 @@ import { Textarea } from '../../components/Textarea'
 import { NoteItem } from '../../components/NoteItem'
 import { Section } from '../../components/Section'
 import { Button } from '../../components/Button'
+import { ButtonText } from '../../components/ButtonText'
 
 import { Container, Form } from "./styles";
+import { api } from "../../services/api";
 
 export function New() {
+  const [title, setTitle] = useState("")
+  const [description, setDescription] = useState("")
+
+  const [links, setLinks] = useState([]);
+  const [newLink, setNewLink] = useState("");
+
+  const [tags, setTags] = useState([]);
+  const [newTag, setNewTag] = useState("");
+
+  const navigate = useNavigate()
+
+  function handleBack() {
+    navigate(-1)
+  }
+
+  function handleAddLink() {
+    setLinks(prevState => [...prevState, newLink]);
+    setNewLink("");
+  }
+
+  function handleRemoveLink(deleted) {
+    setLinks(prevState => prevState.filter(link => link !== deleted))
+  }
+
+  function handleAddTag() {
+    setTags(prevState => [...prevState, newTag]);
+    setNewTag("");
+  }
+
+  function handleRemoveTag(deleted) {
+    setTags(prevState => prevState.filter(tag => tag !== deleted))
+  }
+
+  async function handleNewNote() {
+    if(!title) {
+      return alert("Digite o título da nota.")
+    }
+
+    if(newLink) {
+      return alert("Você deixou uma link no campo para adicionar, mas não clicou em adicionar. Clique para adicionar ou deixe o campo vazio.")
+    }
+
+    if(newTag) {
+      return alert("Você deixou uma tag no campo para adicionar, mas não clicou em adicionar. Clique para adicionar ou deixe o campo vazio.")
+    }
+
+    await api.post("/notes", {
+      title,
+      description,
+      tags,
+      links
+    })
+
+    alert("Nota criada com sucesso!")
+    navigate(-1)
+  }
+
   return (
     <Container>
       <Header />
@@ -18,25 +78,59 @@ export function New() {
         <Form>
           <header>
             <h1>Criar notas</h1>
-            <Link to="/">voltar</Link>
+            <ButtonText onClick={handleBack} title="Voltar"></ButtonText>
           </header>
 
-          <Input placeholder="Título" />
-          <Textarea placeholder="Observações" />
+          <Input
+            placeholder="Título"
+            onChange={e => setTitle(e.target.value)}
+          />
+          <Textarea
+            placeholder="Observações"
+            onChange={e => setDescription(e.target.value)}
+          />
 
           <Section title="Links úteis">
-            <NoteItem value="https://rocketseat.com.br" />
-            <NoteItem isNew placeholder="Novo link" />
+            {
+              links.map((link, index) => (
+                <NoteItem
+                  key={String(index)}
+                  value={link}
+                  onClick={() => handleRemoveLink(link)}
+                />
+              ))
+            }
+            <NoteItem
+              isNew
+              placeholder="Novo link"
+              value={newLink}
+              onChange={e => setNewLink(e.target.value)}
+              onClick={handleAddLink}
+            />
           </Section>
 
           <Section title="Marcadores">
             <div className="tags">
-              <NoteItem value="react" />
-              <NoteItem isNew placeholder="Novo tag" />
+              {
+                tags.map((tag, index) => (
+                  <NoteItem
+                    key={String(index)}
+                    value={tag}
+                    onClick={() => handleRemoveTag(tag)}
+                  />
+                ))
+              }
+              <NoteItem
+                isNew
+                placeholder="Novo tag"
+                value={newTag}
+                onChange={e => setNewTag(e.target.value)}
+                onClick={handleAddTag}
+              />
             </div>
           </Section>
 
-          <Button title="Salvar" />
+          <Button title="Salvar" onClick={handleNewNote} />
         </Form>
       </main>
     </Container>
